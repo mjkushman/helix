8abc0653550ee8c0b16979a6a7dc762316994f30
mjkushman <mjkushman@gmail.com>
2025-02-19 23:20:10 -0800

    vast improvement to rooms, thread, and connecting to socket

--- server/ai.py
+++ server/ai.py
@@ -1,65 +1,33 @@
 import os
+import json
 from dotenv import load_dotenv
-from openai import OpenAI
-
+from openai import OpenAI, AsyncOpenAI
+from sequence import get_sequence
 from utils.format_message_list import format_messages
+from sequence import create_sequence, update_sequence
 
 
 # Load env variables, i.e. API key
 load_dotenv()
 
 # Creates a new assistant
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+async_client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))  # sync client for testing
 
-
-def create_assistant():
-    try:
-        response = client.beta.assistants.create(
-            name="My AI Assistant",
-            instructions='''You're a veteran talent recruiter. Help the user create a sequence of outreach messages.
-                We'll be working with a Sequence object. You have tools to 
-                1) Generate a new sequence entirely
-                2) Add, modify, or delete a step in an existing sequence''',
-            # tools=[{"type": "code_interpreter"}],  # Example tool, customize as needed
-            model="gpt-4o-mini",
-        )
-        return response.id  # Return assistant ID
-    except Exception as e:
-        print(f"Error creating assistant: {str(e)}")
-        return None
-
-
-def fetch_assistant(assistant_id="asst_qgvWGy0bnKkfFcLuWATyfmZ1"):
-    try:
-        response = client.beta.assistants.retrieve(assistant_id)
-
-    except Exception as e:
-        print(f"Error fetching assistant: {str(e)}")
-        return None
-    return response.id
-
-
-def delete_assistant(assistant_id):
-    try:
-        response = client.Assistant.delete(assistant_id)
-        print(f"Assistant {assistant_id} deleted successfully.")
-        return response
-    except Exception as e:
-        print(f"Error deleting assistant: {str(e)}")
-        return None
+assistant_id = "asst_qgvWGy0bnKkfFcLuWATyfmZ1"  # Hardcoded assistant id
 
 
 # Creates a new thread, returns thread_id
 def create_thread():
-    thread = client.beta.threads.create()
-    print(f"CREATED THREAD: {thread}")
+    thread = client.beta.threads.create()  # sync
+    print(f"CREATED THREAD: {thread.id}")
     return thread
 
 
 # Adds a message to a thread
 def add_thread_message(thread_id, role, content):
     try:
-        print("ADDING MESSAGE TO THREAD")
+        print("ADDING MESSAGE TO THREAD", thread_id, role, content)
         client.beta.threads.messages.create(thread_id, role=role, content=content)
     except Exception as e:
         print(f"Error adding message to thread: {str(e)}")
@@ -80,47 +48,68 @@ def fetch_thread_messages(thread_id):
         return None
 
 
-# Initiates a run on this thread and handles polling
-async def init_run(thread_id, assistant_id):
-    try:
-        response = await client.beta.threads.runs.create_and_poll(
-            thread_id=thread_id, assistant_id=assistant_id, poll_interval_ms=2000
-        )
+# Initiates a run on this thread and handles polling. Retrieves the sequence by thread_id and includes it as additional_instructions
+def do_run(thread_id):
+    print('STARTING RUN')
+    sequence = get_sequence(thread_id)
+    print('SEQUENCE:')
+    print(sequence)
 
-        # TODO
+    try:
+        response = client.beta.threads.runs.create_and_poll(
+            thread_id=thread_id,
+            assistant_id=assistant_id,
+            poll_interval_ms=2000,
+            additional_instructions=f'''
+            This is the current sequence: {sequence}''',
+        )
+        print('RUN RESPONSE')
+        print(response.id)
+        print(response.status)
+
         # RUN STATUS: REQUIRES ACTION
-        if run.status == 'requires_action':
+        if response.status == 'requires_action':
             # Handle the function call
-            for tool_call in run.required_action.submit_tool_outputs.tool_calls:
-                if tool_call.function.name == "12345":  # name of your function call
+            for tool_call in response.required_action.submit_tool_outputs.tool_calls:
+                print('TOOL CALL')
+                print(tool_call.function)
+                if (
+                    tool_call.function.name == "create_sequence"
+                ):  # name of your function call
+                    print('REQUESTED CREATE_SEQUENCE TOOL CALL')
 
                     # Arguments returned by llm
                     rawArguments = tool_call.function.arguments
 
                     # response from function calling
-                    response_message = ""
                     try:
                         arguments = json.loads(rawArguments)
 
-                        output = await function_to_be_called(
-                            arguments["firstname"], arguments['phone']
+                        sequence = create_sequence(
+                            thread_id=thread_id, steps=arguments["steps"]
                         )
-                        response_message = output["response"]
+                        print("CREATE SEQUENCE OUTPUT:")
+                        print(f"sequence id: {sequence.id}")
+                        print(F"sequence steps: {sequence.steps}")
+
+                        # tool_output = output["response"]
 
                     except json.JSONDecodeError as e:
-                        response_message = "JSONDecodeError: " + str(e), 400
+                        sequence = "JSONDecodeError: " + str(e), 400
                     except KeyError as e:
-                        response_message = "Missing required argument: {e}", 400
+                        sequence = "Missing required argument: {e}", 400
                     finally:
-
+                        print(f"JSON DUMP steps: {json.dumps(sequence.steps)}")
                         # Send the response back to the function calling tool
-                        run = await client.beta.threads.runs.submit_tool_outputs(
-                            thread_id=run.thread_id,
-                            run_id=run.id,
+                        response = client.beta.threads.runs.submit_tool_outputs(
+                            thread_id=response.thread_id,
+                            run_id=response.id,
                             tool_outputs=[
                                 {
                                     "tool_call_id": tool_call.id,
-                                    "output": response_message,  # pass the response from your function to openai, so it knows if everything worked fine, or happens with me a lot, some arguments was invalid or filled with a placeholder.
+                                    "output": json.dumps(
+                                        sequence.steps
+                                    ),  # pass the response from your function to openai, so it knows if everything worked fine, or happens with me a lot, some arguments was invalid or filled with a placeholder.
                                 }
                             ],
                         )
